d4a75308997c835e7ce4f8a0eed9e6756abe10a6
qkhanh711 <quockhanh7112003@gmail.com>
2024-02-07 16:49:25 +0700

    crawl Sumarization.py

---
+++ crawl_Sumarization.py
@@ -0,0 +1,55 @@
+from bs4 import BeautifulSoup as bs
+import requests as req
+from utils import *
+from tqdm import tqdm
+import argparse
+
+parser = argparse.ArgumentParser(description='Crawl and Summarize news from VnExpress')
+parser.add_argument('--nums_clusters', type=int, default=10, help='Number of clusters to crawl')
+args = parser.parse_args()
+
+nums_clusters = args.nums_clusters
+
+Web = req.get('https://vnexpress.net')
+
+S = bs(Web.text, 'lxml')
+
+navigator = S.find_all(class_='main-nav')
+
+li_elements = navigator[0].find_all('li') 
+classes = [li.get('class')[0] for li in li_elements][1:-1]
+paths = [li.find_all('a')[0].get('href') for li in li_elements][1:-1]
+
+classes.remove('video')
+paths.remove('https://video.vnexpress.net')
+
+print(classes)
+print(paths)
+
+urls = ['https://vnexpress.net' + content for content in paths]
+print(urls)
+
+articles = get_text(urls[18])["List articles' links"]
+print(len(articles))
+articles[10].find_all('a')
+list_to_crawl = [article.find_all('a')[0].get('href')  for article in articles if article.find_all('a') != []]
+print()
+
+pages = [get_text(url) for url in urls]
+
+list_class_to_crawl = []
+for i in range(len(pages)):
+    articles = pages[i]["List articles' links"]
+    list_to_crawl = [article.find('a')['href'] for article in articles if article.find_all('a') != []]
+    list_class_to_crawl.extend(list_to_crawl)
+
+for clusID in tqdm(range(len(list_class_to_crawl[:nums_clusters]))):
+    crawler_Summary([list_class_to_crawl[clusID]], clusID=clusID)
+
+organic_results  = sniper('vnexpress_data_summarization/original/Cluster_001/original/1.txt')
+
+saveClusSummary(organic_results)
+
+for i in range(2,11):
+    organic_results  = sniper(f'vnexpress_data_summarization/original/Cluster_{i:03}/original/1.txt')
+    saveClusSummary(organic_results, ClusID = i)
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup as bs
 import requests as req
+from serpapi import GoogleSearch
+from tqdm import tqdm
 
 def get_text(url):
     Web = req.get(url)
@@ -63,7 +65,7 @@ def crawler_Summary(list_class_to_crawl, clusID =0):
         import os
         if not os.path.exists(f'vnexpress_data_summarization/original/Cluster_{clusID+1:03}/original'):
             os.makedirs(f'vnexpress_data_summarization/original/Cluster_{clusID+1:03}/original')
-        with open(f'vnexpress_data_summarization/original/Cluster_{clusID+1:03}/original/1.txt ', 'a') as f:
+        with open(f'vnexpress_data_summarization/original/Cluster_{clusID+1:03}/original/1.txt', 'a') as f:
             f.write(f"Title: {title}\n")
             f.write(f"Source: {source}\n")
             f.write(f"Link: {url}\n")
@@ -72,3 +74,51 @@ def crawler_Summary(list_class_to_crawl, clusID =0):
             f.write(f"Tags: {tags}\n")
             f.write(f"Summary: {summary}\n")
             f.write(f"Content: {content}\n\n")
+
+def sniper(path):
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        title = lines[0]
+        print(title[6:])
+        params = {
+            "q": f"{title[6:]}",
+            "location": "Vietnam",
+            "hl": "en",
+            "gl": "us",
+            "google_domain": "google.com",
+            "api_key": "d229d82c7ffb85675c13b46b7e28d5b2707a64a3a374f5a535a2f83d85ac3c58"
+            }
+        search = GoogleSearch(params)
+        results = search.get_dict()
+        return results['organic_results']
+    
+def saveClusSummary(organic_results, ClusID = 1):
+    for i in tqdm(range(len(organic_results))):
+        path = f'vnexpress_data_summarization/original/Cluster_{ClusID:03}/original/{i+2}.txt'
+        # print(path)
+        with open(path, 'w') as f:
+            f.write(f'Title: {organic_results[i]["title"]}\n')
+            f.write(f'Source: {organic_results[i]["source"]}\n')
+            f.write(f'Link: {organic_results[i]["link"]}\n')
+            soup = bs(req.get(organic_results[i]["link"]).text, 'lxml')
+            try:
+                published_date = soup.find('span', class_='date').text
+            except:
+                published_date = "N/A"    
+            author = ''
+            content = ''
+            for p in soup.find_all('p'):
+                if p.get('style') == 'text-align:right;':
+                    author = p.text
+                else:
+                    content += p.text
+            f.write(f"Published Date: {published_date}\n")
+            f.write(f'Author: {author}\n')
+            try:
+                tags_string = ', '.join(organic_results[i]["snippet_highlighted_words"])
+            except:
+                tags_string = None
+            f.write(f'Tags: {tags_string}\n')            
+            f.write(f'Summary: {organic_results[i]["snippet"]}\n')
+            f.write(f'Content: {content}\n')
+            
